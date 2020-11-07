51e522fe36ee8baa4b003ab8256a842972c02be8
username anda <freestonsinaga@gmail.com>
2020-11-08 01:17:21 +0700

    Initial commit

---
+++ app/src/main/java/com/example/praktikum_2/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.praktikum_2
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.reflect.KClass
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
